edf577dbfa2004a135134996e1f59cd94b56b118
Francisco Zapata <franciscoreaper@hotmail.com>
2023-01-22 10:35:05 -0500

    Limpieza de importaciones

--- src/hooks/useSendForm.js
+++ src/hooks/useSendForm.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import {axiosInstance} from '../libs/axios';
 import {Alert} from 'react-native';
 import {store} from '../core/store';
